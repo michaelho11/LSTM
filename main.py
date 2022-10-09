2282301dc452d1a277afbd9e049ce4f54da1de5f
Michael <michaelho1156@gmail.com>
2022-10-10 01:33:33 +0800

    First draft of LSTM 1. downloading the stock price as dataframe format 2. normalised the information in scaled by MinMaxScaler

---
+++ main.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+import yfinance as yf
+from sklearn.preprocessing import MinMaxScaler
+
+df = yf.download('0700.HK', start = '2022-01-01')
+
+df = df.tail(10)
+
+print (df.to_string())
+
+scaler = MinMaxScaler(feature_range = (0,1))
+scaled_df = scaler.fit_transform(df)
+
+print (scaled_df)
