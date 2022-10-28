c8b5447cd7c31e39a3786d5305969a62d3bcba55
Michael <michaelho1156@gmail.com>
2022-10-28 14:30:42 +0800

    First draft of LSTM-stock-price-prediction 1. downloading the stock price as dataframe format 2. normalised the information in scaled by MinMaxScaler
    Second draft
    1. data engineering
    2. technical indicators are included
    
    Third draft
    1. LSTM model implementation
    
    Forth draft:
    1. LSTM model modification
    2. Include the technical indicators
    3. drop na for the stock df

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from sklearn import preprocessing
 def dataframe_download (stock, date):
     #download dataframe
     df = yf.download(stock, start = date)
-    df = df.head(50)
+    # df = df.head(100)
     close = df['Close']
     # print (close)
     return df
@@ -29,11 +29,9 @@ def test_train_split(df,ratio,history_point):
     #2. norminalise the dataset and define x_train, y_train, x_test, y_test
     normaliser = preprocessing.MinMaxScaler()
     normalised_train_data = normaliser.fit_transform(train_data_set) #fit = calculated mean/variance
-
-    # print(normalised_train_data)
     x_train = np.array([normalised_train_data[:,0:][i:i+history_point].copy() for i in
                         range(len(normalised_train_data)-history_point)])
-    y_train = np.array([normalised_train_data[:,3][i+history_point].copy() for i in
+    y_train = np.array([normalised_train_data[i+history_point,3].copy() for i in
                         range(len(normalised_train_data)-history_point)])
     y_train = np.expand_dims(y_train,-1)
 
@@ -41,9 +39,8 @@ def test_train_split(df,ratio,history_point):
     x_test = np.array([normalised_test_data[:,0:][i:i+history_point].copy() for i in
                         range(len(normalised_test_data)-history_point)])
     y_test = np.array([test_data_set['Adj Close'][i + history_point].copy() for i in
-                        range(len(normalised_train_data) - history_point)])
+                        range(len(normalised_test_data) - history_point)])
     y_test = np.expand_dims(y_test,-1)
-    # print(y_test)
 
     #3. normalise the actual price
     actual_price_normaliser = preprocessing.MinMaxScaler()
@@ -52,8 +49,9 @@ def test_train_split(df,ratio,history_point):
     nor_adj_close = np.expand_dims(nor_adj_close, 1)
     actual_price_normaliser.fit(nor_adj_close)
 
-    print(x_train)
-    print (x_train.shape)
+    print('---')
+    print(y_test)
+    print(len(x_test))
 
     return x_train, y_train, x_test, y_test, actual_price_normaliser
 
@@ -90,25 +88,53 @@ def add_technical_indicators(stock_df):
 
     return stock_df_copy
 
-def LSTM_model (x_train, y_train, history_point):
-    n_features = 6
-    model = Sequential()
-    model.add(LSTM(21, activation='relu',input_shape=(history_point,n_features), return_sequences=True))
-    model.add(Dropout(0.2))
-    model.compile(optimizer=optimizers.Adam(lr= 0.0008), loss='mse')
-    model.fit(x=x_train, y=y_train,batch_size=15, epochs=170, shuffle=True, validation_split=0.1)
+# def lstm_model (x_train, y_train, history_point):
+#     tf.random.set_seed(10)
+#     n_features = 6
+#     model = Sequential()
+#     model.add(LSTM(21, activation='relu',input_shape=(history_point,n_features)))
+#     # model.add(Dropout(0.2))
+#     model.add(Dense(1))
+#     model.compile(optimizer=optimizers.Adam(learning_rate= 0.0008), loss='mse', metrics=['mean_absolute_error'])
+#     model.fit(x=x_train, y=y_train,batch_size=15, epochs=170, shuffle=True, validation_split=0.1)
+#
+#     return model
+
+def lstm_model(X_train, y_train, history_points):
+    tf.random.set_seed(20)
+    np.random.seed(10)
+    lstm_input = Input(shape=(history_points,11), name='lstm_input')
+
+    inputs = LSTM(21, name='first_layer')(lstm_input)
+    inputs = Dense(1, name='dense_layer')(inputs)
+    output = Activation('linear', name='output')(inputs)
+
+    model = Model(inputs=lstm_input, outputs=output)
+    adam = optimizers.Adam(lr = 0.0008)
+    model.compile(optimizer=adam, loss='mse')
+    model.fit(x=X_train, y=y_train, batch_size=15, epochs=170, shuffle=True, validation_split = 0.1)
 
     return model
 
-
 # print (dataframe_download ('0700.HK', '2022-01-01'))
 stock = dataframe_download('2013.HK', '2022-01-01')
-history_point = 3
+stock = add_technical_indicators(stock)
+stock = stock.dropna()
+history_point = 21
 test_train_split_ratio = 0.2
-new_stock_df = add_technical_indicators(stock)
 x_train, y_train, x_test, y_test, actual_price_normaliser= test_train_split(stock,test_train_split_ratio,history_point)
-LSTM_model(x_train, y_train, history_point)
-# print(new_stock_df.to_string())
+model = lstm_model(x_train, y_train, history_point)
+
+y_pred = model.predict(x_test)
+print(y_pred)
+print('--')
+predicted_price = actual_price_normaliser.inverse_transform(y_pred)
+print(predicted_price)
+print('---')
+
+
+
+
 
 
 
